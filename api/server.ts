549fad1dd12e15c85dc35f56d671c23d44c28653
ECBetaG4B3 <contact@ECBetaSolutons.com>
2025-12-07 16:32:02 -0500

    cleanup

--- api/server.ts
+++
@@ -1,7 +0,0 @@
-import { ServerInfo } from '../types/server';
-
-export async function fetchServerInfo(): Promise<ServerInfo> {
-  // Replace with real API call
-  const res = await fetch('/api/server/info');
-  return res.json();
-}
